de4ebfac9e0ba360523cfeb72a6b5629601a192d
iED <ied@outlook.com.au>
2015-05-12 19:50:52 +1000

    Add incrementCounter function and tied to button object (addTarget)

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
-        
         self.view.addSubview(label)
-        
+        self.label = label
         
         var button = UIButton()
         button.frame = CGRectMake(150, 250, 80, 80)
         button.setTitle("Click", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        
         self.view.addSubview(button)
         
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+    }
+    
+    func incrementCount() {
+        self.count++
+        self.label.text="\(self.count)"
         
     }
 
